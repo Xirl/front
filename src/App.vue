fbe0b7d6d9177a2ddac24f2eb08e29cbb91dd44d
细雪 <1981486693@qq.com>
2022-10-25 14:37:10 +0800

    s

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-for="(user) in users" :key="user.id">
+      {{user.id}}:{{user.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'app',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then(response=>{
+      this.users = response.data;
+    });
+  }
+}
+</script>
+
+<style>
+</style>
